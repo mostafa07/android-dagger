e0456b5764c9a0d79d73194880a67bb1e2655d28
m.salem <msalem@ejada.com>
2023-05-05 16:34:36 +0300

    injected dependencies for main and registration flows using dagger

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,23 @@
+package com.example.android.dagger.di
+
+import android.content.Context
+import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Component(modules = [StorageModule::class])
+@Singleton
+interface AppComponent {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+    fun inject(activity: MainActivity)
+    fun inject(activity: RegistrationActivity)
+}
