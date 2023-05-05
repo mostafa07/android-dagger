f472f1b492014900ea18c5e1ffd1c69f8ccf8238
m.salem <msalem@ejada.com>
2023-05-05 18:39:25 +0300

    injected dependencies for registration flow using dagger and added registration subcomponent

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -2,14 +2,11 @@ package com.example.android.dagger.di
 
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
-import com.example.android.dagger.registration.RegistrationActivity
-import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
-import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
-@Component(modules = [StorageModule::class])
+@Component(modules = [AppSubcomponents::class, StorageModule::class])
 @Singleton
 interface AppComponent {
 
@@ -18,6 +15,9 @@ interface AppComponent {
         fun create(@BindsInstance context: Context): AppComponent
     }
 
+
+    fun registrationComponent(): RegistrationComponent.Factory
+
+
     fun inject(activity: MainActivity)
-    fun inject(activity: RegistrationActivity)
 }
